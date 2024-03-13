3e84e35bb245fb42ab1693fb77ad3e71ed6e9186
jotalouzeiro <159553412+MichaelMMDev@users.noreply.github.com>
2024-03-13 11:59:11 -0300

    estudojs
    pipipopo

---
+++ index.js
@@ -0,0 +1,8 @@
+const notaDoAluno1 =5;
+const notaDoaluno2 = 8;
+const notaDoaluno3 = 8;
+
+const nomeDoAluno1 = "joão"
+const nomeDoAluno2 = "pedrinho"
+
+const media = (notaDoAluno1+nomeDoAluno2+notaDoaluno3/2)
