5669db1adb78a1bd92a5da74fafe1217d6804d4b
MichaelMMDev <159553412+MichaelMMDev@users.noreply.github.com>
2024-04-01 19:12:31 -0300

    desafiojs
    desafio de lógica em js

--- desafio01.js
+++ desafio01.js
@@ -1,8 +1,14 @@
-const Idade= 22
+// && siguinifica E, as duas condições devem ser verdadeiras
+// || siguinifica OU, ou seja somente uma das condições necessitam ser verdadeiras
+// ! nega uma condição
 
-if (Idade >= 18) {
+const Idade= 2
+const permissao= 'sim'
+const permissao01= 'nao'
+
+if (Idade >= 18 && permissao == permissao01  ) {
     console.log('Você pode entrar!')}
 else{ 
-console.log('volte quando tiver 18 anos ')}
+console.log(`Volte quando tiver 18 anos e/ou permissão igual a ${permissao}`)}
 
 
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // str = a nome, templare str = nome2 e nome3, na templare eu posso chamar outras variáveis.
 
 const nome= 'mchael22';
@@ -35,16 +34,121 @@ if (media >5  ){
 else{
     console.log(`O resultado da turma não foi satisfatório: ${media}, a turma deve estudar mais.`)
 }
-=======
-const notaDoAluno1 =5;
-const notaDoaluno2 = 8;
-const notaDoaluno3 = 8;
 
-const nomeDoAluno1 = "joão"
-const nomeDoAluno2 = "pedrinho"
+// iniciando objetos:
+//os objetos vêm solucionar os casos em que  temos varias informações relacionadas a uma mesma variável.
 
-const media = (notaDoAluno1+nomeDoAluno2+notaDoaluno3/2)
-console.log(media)
+const nomeAluna ='Lais'
+const endereço =' casa 10, rua 11' 
+const telefone = 86999988734 
+const nota = 8
+const sala = 2 
+const turno = 'manhãm'
 
-console.log(nomeDoAluno1)
->>>>>>> 08055242ca53584b92e54e15638304a247923cab
+// o objeto pode guardar todas as informações da Lais dentro dele, e quando quisermos chamar alguma delas, só "falamos" o nome Lais
+// esse conceito, de objeto, se assemelha muito ao de variável, e entendemos variável como uma caixinha na memoria do pc
+// entenderemos agora o objeto, que é uma guarda roupa ou um armário, chio de gavetas, em suma, o objeto só consegue armazenar mais coisas que uma variável.
+
+
+// estrutura dos objetos:
+
+const lais = {
+    endereço:' casa 10, rua 11',
+    telefone: 86999988734,
+    nota: 8,
+    sala: 2,
+    turno: 'manhãm'
+}
+console.log(lais.nota)
+
+// tudo que está dentro do objeto chamamos de propiedades; usamos '{}' para iniciar o objeto; usamos ',' para separar as propiedades; usamos '.' para acessar as propiedades
+
+
+// estrutura de arrays
+// ela guarda vários objetos em uma variável só
+
+// receita em arrays
+
+const nescal={
+    ferramentas1:'uma chaleira, um copo, uma colher de sopa',
+    engredientes1:'Nescal: 50g, Leite: 60g, água: 50ml de água',
+    modoDePreparo1:'ferva a água, misture o leite e o nescal em um copo, coloque a água dentro do copo e mecha. Seu nescal está pronto!'
+
+}
+
+const paoComOvo={
+    ferramentas2:'uma frigideira, uma colher, um prato.',
+    engredientes2:'um pão massa grossa, 2 ovos, 20g de manteiga, 1g de sal',
+    modoDePreparo2:'pegue a frigideira, coloque-a para esquentar a fofo médio no fogão, despeje a manteiga com uma colher, e espere derreter, espalhe a manteiga com a colher, quebre os ovos dentro da panela e adcione o sal, mecha os ovos até ficar em um ponto aguadavel a você '
+}
+
+const cafe= {
+    ferramentas3:'uma chaleira, uma xícara, uma colher, um coador de café',
+    engredientes3:'300ml de água, 1 colher de pó de café, meia colher de açucar',
+    modoDePreparo3:' despeje a água na chaleira, coloque-os em fogo médio, quando a aguá ferver desligue o fogo e acrescente o pó de café, mecha a mistura, após mecher bem despeje o café na chicará por meio do coador, coloque o tanto que desejar na chicará, após isso, adicioneo açucar, pronto, seu café está pronto '
+}
+
+const beju= {
+    ferramentas4:'uma frigideira, uma colher, um prato ',
+    engredientes4:'2 punhados de goma fresca, uma pitada de sal, 50g de manteiga ',
+    modoDePreparo4:'coloque a frigideira em fogo alto, despeje a massa e espalhe-a, quando a massa soltar da panela vire-a e deixe o outro lado dourar, após algusn minutos, quando as duas partes estiverem douradas, coloque-o no prato e passe  a manteiga, enrrole o beju e estará pronto '
+} 
+
+const paoComManteiga={
+    ferramentas5:'umprato, uma colher',
+    engredientes5:'um pão, 20g de manteiga',
+    modoDePreparo5:'pegue o pão e espalhe a manteiga, pronto seu pão está pronto '
+}
+
+const cafeDaManham=[
+    {ferramentas2:'uma frigideira, uma colher, um prato.',
+engredientes2:'um pão massa grossa, 2 ovos, 20g de manteiga, 1g de sal',
+modoDePreparo2:'pegue a frigideira, coloque-a para esquentar a fofo médio no fogão, despeje a manteiga com uma colher, e espere derreter, espalhe a manteiga com a colher, quebre os ovos dentro da panela e adcione o sal, mecha os ovos até ficar em um ponto aguadavel a você '},
+
+{ferramentas2:'uma frigideira, uma colher, um prato.',
+engredientes2:'um pão massa grossa, 2 ovos, 20g de manteiga, 1g de sal',
+modoDePreparo2:'pegue a frigideira, coloque-a para esquentar a fofo médio no fogão, despeje a manteiga com uma colher, e espere derreter, espalhe a manteiga com a colher, quebre os ovos dentro da panela e adcione o sal, mecha os ovos até ficar em um ponto aguadavel a você '},
+
+{ferramentas3:'uma chaleira, uma xícara, uma colher, um coador de café',
+engredientes3:'300ml de água, 1 colher de pó de café, meia colher de açucar',
+modoDePreparo3:' despeje a água na chaleira, coloque-os em fogo médio, quando a aguá ferver desligue o fogo e acrescente o pó de café, mecha a mistura, após mecher bem despeje o café na chicará por meio do coador, coloque o tanto que desejar na chicará, após isso, adicioneo açucar, pronto, seu café está pronto '},
+
+{ferramentas4:'uma frigideira, uma colher, um prato ',
+engredientes4:'2 punhados de goma fresca, uma pitada de sal, 50g de manteiga ',
+modoDePreparo4:'coloque a frigideira em fogo alto, despeje a massa e espalhe-a, quando a massa soltar da panela vire-a e deixe o outro lado dourar, após algusn minutos, quando as duas partes estiverem douradas, coloque-o no prato e passe  a manteiga, enrrole o beju e estará pronto '},
+
+{ ferramentas5:'umprato, uma colher',
+engredientes5:'um pão, 20g de manteiga',
+modoDePreparo5:'pegue o pão e espalhe a manteiga, pronto seu pão está pronto '},
+]
+
+console.log(cafeDaManham[2].ferramentas3, cafeDaManham[2].engredientes3, cafeDaManham[2].modoDePreparo3)
+
+
+// quando quisermos usar um código que ja escrevemos podemos chama-lo através de uma função
+//só precisamos previamente declara-la e na h de usar é só chamar 
+
+function soma (a,b){
+    return a+b
+}
+
+console.log(soma (5,6))
+
+// toda função tem um comando(function, um nome(soma), os parametros dentro de parenteses(a,b...),e dentro dos cochetes vão os comandos, dados pelo nome (return)
+
+function juntar (a,b){
+    return a+b
+}
+console.log(juntar (8,7))
+
+//estruturas de repetição 
+// existe o comando 'for', ele é responsavel por repetir um código até que uma condição seja verdadeira.
+//para isso, devemos usar uma nova variável chamada 'let', ela é diferente de 'const' pois pode ser auterada durante a execução do código.
+
+for( let i=0 ; i<9 ; i++){
+    console.log(i)
+}
+
+for (let idade= 8; idade<56; idade++){
+    console.log(idade)
+}
